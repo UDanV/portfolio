eabb595732e3f9b07a20c779e532dc8ed0972d07
UDanV <danil17092006@gmail.com>
2024-05-23 22:59:40 +0300

    created repository

---
+++ src/js/modules/Background.js
@@ -0,0 +1,15 @@
+import '../libs/three.min.js';
+import '../libs/vanta.net.min.js';
+
+// Animated 3D Background
+VANTA.NET({
+  el: "#promo",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x993fff
+})
---
+++ src/js/script.js
@@ -0,0 +1,57 @@
+// Libs
+import './libs/jquery-3.6.1.min.js';
+
+// Modules
+import './modules/Background.js';
+import './modules/Menu.js';
+import './modules/Portfolio.js';
+import './modules/Privacy.js';
+
+// Plugins
+import './plugins/jquery.animateTyping.js';
+import './plugins/jquery.anchorScroll.js';
+import './plugins/jquery.simpleLoadMore.min.js';
+import './plugins/jquery.showMore.js';
+import tabs from './plugins/jquery.tabs.js';
+
+// AnchorScroll
+var anchorScroll = $(document).AnchorScroll({
+  target: 'a[data-link]',
+});
+
+// Simple Load More
+$('.portfolio__wrapper.all').simpleLoadMore({
+  item: '.portfolio__content',
+  count: 8,
+  itemsToLoad: window.innerWidth >= 600 ? 8 : 4,
+  btnHTML: '<a href="#" class="portfolio__content-link btn">Еще проекты</a>',
+});
+
+// ShowMore.js
+$(document).ready(function () {
+  $('#test-last').showMore({
+    // minheight: 1024,
+    buttontxtmore: '&#8595;',
+    buttontxtless: '&#8593;',
+    animationspeed: 750,
+  });
+});
+
+// Modal
+$('.modal__close').on('click', function () {
+  $('.overlay, #thanks').fadeOut('fast');
+});
+
+// Pageup
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 1600) {
+    $('.pageup').fadeIn();
+  } else {
+    $('.pageup').fadeOut();
+  }
+});
+
+// jQuery Tabs
+tabs('resume__nav', 'resume__elem_active', 'resume__column');
+tabs('skills__nav', 'skills__elem_active', 'skills__item');
+tabs('portfolio__nav', 'portfolio__item_active', 'portfolio__wrapper');
